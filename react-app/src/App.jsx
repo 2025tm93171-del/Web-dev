20c0b399bf24046108a17805baba0a25808fc9c8
vaishnavi-bits <2025tm93171@wilp.bits-pilani.ac.in>
2026-09-10 20:57:58 +0530

    Add React app version 1

---
+++ react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Development Project</h1>
+      <p>React Application - Version 1.0</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
